f19f13b529cbd0db7920113985f151cd871f8b73
tca0006medac <tca0006@alu.medac.es>
2025-10-03 11:43:52 +0200

    Update login.php

--- docs/login.php
+++ docs/login.php
@@ -3,7 +3,7 @@ session_start();
 require_once "conexion.php";
 
 if ($_SERVER["REQUEST_METHOD"] !== "POST") {
-    header("Location: login.html");
+    header("Location: login.php");
     exit;
 }
 
@@ -35,4 +35,5 @@ if ($res && $res->num_rows === 1) {
     echo "❌ Usuario no encontrado";
 }
 ?>
+
 
